2ed5626ade247b8ec8744b82e2c6c820c5a150ee
Gary Briggs <chunky@icculus.org>
2011-03-20 19:07:56 -0700

    Commented the meta sql sample file

--- meta.sql
+++ meta.sql
@@ -1,12 +1,17 @@
 PRAGMA foreign_keys=ON;
 BEGIN;
 
+-- You can group tables into any number of "clusters"
+--   For example, "structure" and "user content", or "input" and "output"
+--   At time of writing, the color is the outline for the box
 CREATE TABLE IF NOT EXISTS cluster (
 	clusterid INTEGER PRIMARY KEY,
 	label TEXT,
 	color TEXT
 );
 
+-- Put each table that needs to go into a cluster into here
+--   Tables not mentioned here are simply not added to a cluster
 CREATE TABLE IF NOT EXISTS tbl_cluster (
 	tbl_name TEXT NOT NULL,
 	clusterid INTEGER NOT NULL,
@@ -22,6 +27,7 @@ INSERT INTO tbl_cluster (tbl_name, clusterid) VALUES ("cluster", (SELECT cluster
 INSERT INTO tbl_cluster (tbl_name, clusterid) VALUES ("ignorelist", (SELECT clusterid FROM cluster WHERE label="Red Stuff"));
 
 
+-- These are simply put in as-is as graph-wide attributes
 CREATE TABLE IF NOT EXISTS graphsettings (
 	setting TEXT NOT NULL
 );
@@ -31,10 +37,12 @@ INSERT INTO graphsettings (setting) VALUES ("splines=true");
 INSERT INTO graphsettings (setting) VALUES ("overlap=false");
 
 
+-- Tables mentioned here aren't included in the output
 CREATE TABLE IF NOT EXISTS ignorelist (
 	tbl_name TEXT NOT NULL
 );
 
+-- These are created by ANALYZE
 INSERT INTO ignorelist (tbl_name) VALUES ("sqlite_stat1");
 INSERT INTO ignorelist (tbl_name) VALUES ("sqlite_stat2");
 
